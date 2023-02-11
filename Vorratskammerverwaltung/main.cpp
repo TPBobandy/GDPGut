830512107a56d6276c437fe3a24f2a48df6b195d
TPBobandy <63726348+TPBobandy@users.noreply.github.com>
2023-02-11 11:38:26 +0100

    Add files via upload

--- Vorratskammerverwaltung/main.cpp
+++ Vorratskammerverwaltung/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <sstream>
 #include <ctime>
+#include <cmath>
 
 
 class Vorratskammer {
@@ -11,10 +12,10 @@ class Vorratskammer {
     std::vector < std::string > dinge;
     std::vector < std::string > abldaten;
     std::vector < int >         mengen;
-    std::vector<std::tm>        dates;
+    std::vector < std::tm >     dates;
     std::vector < std::time_t > uhrzeit;
     std::string ding;
-
+    
     void einlesen(){
         
         std::ifstream file("Vorratskammer.txt");
@@ -35,38 +36,44 @@ class Vorratskammer {
 
         
         file.close();
-
-        
-        
-
-      
-
        
     
 }
     void zeit()
-    {
-        
-    for (const auto& date_string : abldaten) {
+    {   
+    int i = 0;
+    for (const auto& date_string : abldaten) {          
     std::istringstream ss(date_string);
     int year, day, month;
-    ss >> year >> day >> month;
+    ss >> year >> month >> day;
 
     std::tm date = {0};
-    date.tm_year = year - 1900;  // tm_year is years since 1900
-    date.tm_mon = month - 1;     // tm_mon is 0-based
+    date.tm_year = year - 1900;
+    date.tm_mon = month - 1;
     date.tm_mday = day;
-    
     dates.push_back(date);
-  }
-
-  std::cout << "The dates are:\n";
-  for (const auto& date : dates) {
-    std::cout << std::asctime(&date) << '\n';
-  }
 
     }
 
+    std::time_t now = std::time(nullptr);
+    std::tm now_tm = *std::localtime(&now);
+     
+        for (std::tm date : dates) {
+        
+       
+        
+        std::time_t date_time = std::mktime(&date);
+        double diff = std::difftime(date_time, now);
+        int diff_days = std::floor(diff / (60 * 60 * 24));
+        i++;
+
+        if (diff_days < 7) {
+        std::cout << dinge[i-1] << " wird bald Ablaufen bitte verbrauchen sie diese bald\n";
+            }
+        
+        }
+    }
+   
     int hinzufügen()
     {
         
@@ -81,7 +88,7 @@ class Vorratskammer {
                 std::cin >> y >> m >> d;             
                 std::cout << "Bitte geben sie die Menge des Gegenstands an\n";
                 std::cin >> z;
-                    
+
 
                 std::ofstream out;
                     out.open("Vorratskammer.txt", std::ofstream::app);
@@ -131,46 +138,51 @@ class Vorratskammer {
 
     return 0;
     }
+
+
+    void menu() {
+        
+        
+        
+    int choice;
+        
+            std::cout << "Willkommen bei ihrer Vorratskammer\n\n";
+        do
+        {
+            std::cout << "0. Verlassen\n1.Gegenstand hinzufügen\n2.Dinge verbrauchen oder entfernen\n3.Lassen sie sich Gespeicherte gegenstände anzeigen\n4.Lassen sie sich ausgeben welche Gegenstände bald auslaufen";
+            std::cin >> choice;
+
+
+            switch(choice)
+        {
+            case 0:
+                std::cout << "Auf wiedersehen\n";
+                
+            case 1:            
+            hinzufügen();
+            break;
+            case 2:
+            entfernen();
+                break;
+            case 3:
+            anzeigen();
+                break;
+            case 4:
+            zeit();
+            break;
+        }
+    }
+      while (choice != 0);
+
+      
+    }  
 };
 
-// int menu() {
-   
-//    int choice;
-
-//         std::cout << "Willkommen bei ihrer Vorratskammer\n\n";
-//     do
-//     {
-//         std::cout << "0. Verlassen\n1.Gegenstand hinzufügen\n2.Dinge verbrauchen oder entfernen\n3.Gegenstand entfernen\n4.Lassen sie sich alle gespeicherten gegenstände anzeigen\n";
-//         std::cin >> choice;
-
-
-//         switch(choice)
-//     {
-//         case 0:
-//             std::cout << "Auf wiedersehen\n";
-//             return 0;
-//         case 1:            
-//           vk.hinzufügen();
-//            break;
-//         case 2:
-//           vk.verbrauchen();
-//             break;
-//         case 3:
-//           vk.entfernen();
-//             break;
-//         case 4:
-//            vk.anzeigen();
-//             break;
-//     }
-// }
-//      while (choice != 0);
-
-//      return 0;
-// }  
- 
 int main(){
-Vorratskammer vk;
-    vk.anzeigen();
+    Vorratskammer vk;
+    vk.einlesen();
+    vk.menu();
+
     
    
 }
